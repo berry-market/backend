82e1dc6152d79c4025a18344f1beeab3c124a30d
drinkgalaxy <drinkgalaxy1226@gmail.com>
2025-01-02 14:25:21 +0900

    fix: 엔티티에 접근 제어자 추가

--- post/src/main/java/com/berry/post/domain/model/Post.java
+++ post/src/main/java/com/berry/post/domain/model/Post.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "post")
 public class Post {
 
--- post/src/main/java/com/berry/post/domain/model/PostCategory.java
+++ post/src/main/java/com/berry/post/domain/model/PostCategory.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "post_category")
 public class PostCategory extends BaseEntity{
 
