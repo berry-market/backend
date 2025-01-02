e811f0d1bc2fcc21ca10bf460bcec17e9a39f592
drinkgalaxy <drinkgalaxy1226@gmail.com>
2025-01-02 16:22:37 +0900

    feature: 게시글 카테고리 전체 조회

--- post/src/main/java/com/berry/post/application/service/PostCategoryService.java
+++ post/src/main/java/com/berry/post/application/service/PostCategoryService.java
@@ -5,8 +5,13 @@ import com.berry.common.response.ResErrorCode;
 import com.berry.post.domain.model.PostCategory;
 import com.berry.post.domain.repository.PostCategoryRepository;
 import com.berry.post.presentation.request.postCategory.PostCategoryCreateRequest;
+import com.berry.post.presentation.response.postCategory.PostCategoryResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,5 +32,25 @@ public class PostCategoryService {
         .build();
 
     postCategoryRepository.save(postCategory);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<PostCategoryResponse> getPostCategories(String keyword, Pageable pageable) {
+
+    int size = pageable.getPageSize();
+    Sort sort = pageable.getSort().isSorted() ? pageable.getSort() : Sort.by(
+        Sort.Order.desc("createdAt")
+    );
+    pageable = PageRequest.of(pageable.getPageNumber(), size, sort);
+
+    Page<PostCategory> categories;
+    if (keyword.isEmpty()) {
+      categories = postCategoryRepository.findAllByDeletedYNFalse(pageable);
+
+    } else {
+      categories = postCategoryRepository.findAllByCategoryNameAndDeletedYNFalse(pageable, keyword);
+    }
+
+    return categories.map(PostCategoryResponse::new);
   }
 }
