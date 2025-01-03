593001387b56427f48e5e2a1696becdae4b32fef
ksngh <chzhqk98@naver.com>
2025-01-03 15:11:29 +0900

    feat : bid 채팅에서 입찰 가격 받아서 redis에 올리기  (#7)

--- bid/src/main/java/com/berry/bid/infrastructure/config/QuerydslConfig.java
+++ bid/src/main/java/com/berry/bid/infrastructure/config/QuerydslConfig.java
@@ -4,11 +4,14 @@ package com.berry.bid.infrastructure.config;
 import com.querydsl.sql.Configuration;
 import com.querydsl.sql.MySQLTemplates;
 import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.codegen.MetaDataExporter;
 import com.querydsl.sql.spring.SpringConnectionProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.sql.SQLException;
 
 @org.springframework.context.annotation.Configuration
 @RequiredArgsConstructor
@@ -17,9 +20,18 @@ public class QuerydslConfig {
     private final DataSource dataSource;
 
     @Bean
-    public SQLQueryFactory queryFactory() {
+    public SQLQueryFactory queryFactory() throws SQLException {
         Configuration configuration = new Configuration(new MySQLTemplates()); // MySQL Dialect 설정
         return new SQLQueryFactory(configuration, new SpringConnectionProvider(dataSource));
+    }
+
+    @Bean
+    public MetaDataExporter exporter() throws SQLException {
+        MetaDataExporter exporter = new MetaDataExporter();
+        exporter.setPackageName("com.berry.bid.querydsl");
+        exporter.setTargetFolder(new File("src/main/generated"));
+        exporter.export(dataSource.getConnection().getMetaData());
+        return exporter;
     }
 
 }
--- bid/src/main/java/com/berry/bid/presentation/controller/external/BidChatController.java
+++ bid/src/main/java/com/berry/bid/presentation/controller/external/BidChatController.java
@@ -4,11 +4,11 @@ import com.berry.bid.application.dto.bidchat.BidChatCreate;
 import com.berry.bid.application.model.cache.BidChat;
 import com.berry.bid.domain.service.BidChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,9 +17,10 @@ public class BidChatController {
     private final BidChatService bidChatService;
     private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/api/bids")
-    public void createBidChat(@RequestParam Long postId,
-                              @RequestBody BidChatCreate.Request bidChatCreate) {
+    //TODO : post man에서 test 필요 ..
+    @MessageMapping("/api/v1/post/{postId}/bids-chat")
+    public void createBidChat(@DestinationVariable Long postId,
+                              @Payload BidChatCreate.Request bidChatCreate) {
 
         //TODO : user 검증 시 바꾸기
         Long bidderId = 1L;
@@ -28,7 +29,7 @@ public class BidChatController {
         BidChat bidchat = bidChatService.createBidChat(postId, bidChatCreate, bidderId);
         BidChatCreate.Response response = BidChatCreate.Response.of(bidchat.getAmount(), bidderNickname, bidchat.getCreatedAt());
 
-        messagingTemplate.convertAndSend("/topic/bids/" + postId, response);
+        messagingTemplate.convertAndSend("/topic/bids-chat/" + postId, response);
 
     }
 
