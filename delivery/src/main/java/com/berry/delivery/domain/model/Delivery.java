078844f6c2c4d1ed097dc3aa61a2501b14e3311f
Hxxseung <soaorsith2@naver.com>
2025-01-03 11:02:58 +0900

    fix : Setter delete

--- delivery/src/main/java/com/berry/delivery/domain/model/Delivery.java
+++ delivery/src/main/java/com/berry/delivery/domain/model/Delivery.java
@@ -15,23 +15,18 @@ public class Delivery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long deliveryId;
 
-    @Setter
     @Column(nullable = false)
     private Long receiverId;
 
-    @Setter
     @Column(nullable = false)
     private Long senderId;
 
-    @Setter
     @Column(nullable = false)
     private Long bidId;
 
-    @Setter
     @Column(nullable = false)
     private String address;
 
-    @Setter
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
