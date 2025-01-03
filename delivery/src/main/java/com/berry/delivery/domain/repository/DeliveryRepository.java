f8aa8e44ab299bfe998544f5cc3a3ba9b1813ce1
Hxxseung <soaorsith2@naver.com>
2025-01-03 14:22:46 +0900

    feat : Read func feat

--- delivery/src/main/java/com/berry/delivery/domain/repository/DeliveryRepository.java
+++ delivery/src/main/java/com/berry/delivery/domain/repository/DeliveryRepository.java
@@ -2,6 +2,9 @@ package com.berry.delivery.domain.repository;
 
 import java.util.Optional;
 import com.berry.delivery.domain.model.Delivery;
+import com.berry.delivery.domain.model.DeliveryStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- delivery/src/main/java/com/berry/delivery/presentation/controller/DeliveryController.java
+++ delivery/src/main/java/com/berry/delivery/presentation/controller/DeliveryController.java
@@ -3,14 +3,17 @@ package com.berry.delivery.presentation.controller;
 import com.berry.common.response.ApiResponse;
 import com.berry.common.response.ResSuccessCode;
 import com.berry.delivery.application.DeliveryService;
+import com.berry.delivery.domain.model.DeliveryStatus;
+import com.berry.delivery.presentation.dto.DeliveryDto;
 import com.berry.delivery.presentation.dto.request.DeliveryCreateRequest;
+import com.berry.delivery.presentation.dto.response.DeliverySearchResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,4 +28,13 @@ public class DeliveryController {
         return ResponseEntity.ok(ApiResponse.OK(ResSuccessCode.CREATED, "배송이 생성되었습니다."));
     }
 
+    @GetMapping
+    public ResponseEntity<ApiResponse<?>> getALlDelivery(Pageable pageable) {
+        Page<DeliveryDto> deliveries = deliveryService.getAllDelivery(pageable);
+        return ResponseEntity.ok(ApiResponse.OK(ResSuccessCode.READ, deliveries));
+    }
+   @GetMapping("/{deliveryId}")
+    public ResponseEntity<ApiResponse<?>> getDelivery(@PathVariable Long deliveryId) {
+        return ResponseEntity.ok(ApiResponse.OK(ResSuccessCode.READ, deliveryService.getDelivery(deliveryId)));
+   }
 }
