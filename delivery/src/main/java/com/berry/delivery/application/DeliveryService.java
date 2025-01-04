688b1c21171422a5fa5757d83c7604ac7adc4a5a
Hxxseung <soaorsith2@naver.com>
2025-01-04 21:55:28 +0900

    feat : 수정 기능 구현

--- delivery/src/main/java/com/berry/delivery/application/DeliveryService.java
+++ delivery/src/main/java/com/berry/delivery/application/DeliveryService.java
@@ -3,6 +3,7 @@ package com.berry.delivery.application;
 import com.berry.common.exceptionhandler.CustomApiException;
 import com.berry.common.response.ResErrorCode;
 import com.berry.delivery.domain.model.Delivery;
+import com.berry.delivery.domain.model.DeliveryStatus;
 import com.berry.delivery.domain.repository.DeliveryRepository;
 import com.berry.delivery.presentation.dto.DeliveryDto;
 import com.berry.delivery.presentation.dto.request.DeliveryCreateRequest;
@@ -42,10 +43,43 @@ public class DeliveryService {
     }
 
     @Transactional
+    public void updateDelivery(Long deliveryId, DeliveryUpdateRequest req) {
+        Delivery delivery = deliveryRepository.findByDeliveryId(deliveryId)
+                .orElseThrow(()->new CustomApiException(ResErrorCode.BAD_REQUEST,"존재하지 않는 배송입니다."));
+
+        validateStatusTransition(delivery.getStatus(), req.status());
+
+        delivery = Delivery.builder()
+                .deliveryId(deliveryId)
+                .receiverId(req.receiverId())
+                .senderId(req.senderId())
+                .bidId(req.bidId())
+                .address(req.address())
+                .status(req.status())
+                .build();
+
+        deliveryRepository.save(delivery);
+    }
+
+    @Transactional
     public void deleteDelivery(Long deliveryId) {
         Delivery delivery = deliveryRepository.findById(deliveryId)
                 .orElseThrow(()->new CustomApiException(ResErrorCode.BAD_REQUEST,"존재하지 않는 배송입니다."));
 
         delivery.markAsDeleted();
+    }
+
+    private void validateStatusTransition(DeliveryStatus currentStatus, DeliveryStatus newStatus) {
+        if (currentStatus == DeliveryStatus.DONE) {
+            throw new CustomApiException(ResErrorCode.BAD_REQUEST, "완료된 배송은 상태를 변경할 수 없습니다.");
+        }
+
+        if (currentStatus == DeliveryStatus.STARTED && newStatus == DeliveryStatus.READY) {
+            throw new CustomApiException(ResErrorCode.BAD_REQUEST, "이미 시작된 배송을 준비 상태로 되돌릴 수 없습니다.");
+        }
+
+        if (newStatus == DeliveryStatus.DONE && currentStatus != DeliveryStatus.STARTED) {
+            throw new CustomApiException(ResErrorCode.BAD_REQUEST, "배송이 시작되지 않은 상태에서는 완료할 수 없습니다.");
+        }
     }
 }
