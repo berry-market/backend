75aa4a385596cb8f5891d7a11fb11f9e997f6486
ksngh <chzhqk98@naver.com>
2025-01-08 18:30:05 +0900

    refactor : 응답에 맞게 dto 수정 (#68)

--- bid/src/main/java/com/berry/bid/infrastructure/model/dto/PostInternalView.java
+++ bid/src/main/java/com/berry/bid/infrastructure/model/dto/PostInternalView.java
@@ -1,16 +1,31 @@
 package com.berry.bid.infrastructure.model.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 public class PostInternalView {
 
     @Getter
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class Response{
+        private Long postCategoryId;
+        private Long writerId;
         private String productName;
-        private String productURL;
+        private String productContent;
+        private Integer immediatePrice;
+        private Integer startedPrice;
+        private LocalDateTime auctionStartedAt;
+        private LocalDateTime auctionEndedAt;
+        private String productStatus;
+        private String deliveryMethod;
+        private Integer deliveryFee;
+        private String productImage;
+        private Integer likeCount;
+        private Integer viewCount;
     }
 
 }
