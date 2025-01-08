baae35b3df7fb0f325a7b0f497d0fa73209436fb
유현 <yh7119@gmail.com>
2025-01-08 12:15:43 +0900

    feat : feignClient 활성화

--- user/src/main/java/com/berry/user/UserApplication.java
+++ user/src/main/java/com/berry/user/UserApplication.java
@@ -2,11 +2,13 @@ package com.berry.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {
     "com.berry.user",
     "com.berry.common"
 })
+@EnableFeignClients
 public class UserApplication {
 
     public static void main(String[] args) {
