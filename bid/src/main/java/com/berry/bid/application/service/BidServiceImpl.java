09183b3725873efb6ed2eba39c089c9a1d7f58fe
ksngh <chzhqk98@naver.com>
2025-01-08 12:52:55 +0900

    feat : 경매 종료 시 post로 가격 전달 (#40)

--- bid/src/main/java/com/berry/bid/application/service/BidServiceImpl.java
+++ bid/src/main/java/com/berry/bid/application/service/BidServiceImpl.java
@@ -1,12 +1,12 @@
 package com.berry.bid.application.service;
 
 import com.berry.bid.application.model.event.PostEvent;
+import com.berry.bid.application.service.message.BidProducerService;
 import com.berry.bid.domain.model.entity.Bid;
 import com.berry.bid.domain.repository.BidChatRepository;
 import com.berry.bid.domain.repository.BidRepository;
 import com.berry.bid.domain.service.BidService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,25 +14,31 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BidServiceImpl implements BidService {
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private static final String bidChatKey = "post:";
+    private final BidProducerService bidProducerService;
     private final BidChatRepository bidChatRepository;
     private final BidRepository bidRepository;
-    private static final String bidChatKey = "post:";
 
     @Override
     @Transactional
     public void createBid(PostEvent.Close event) {
-        bidRepository.save(eventToBid(event));
+        Bid bid = eventToBid(event);
+        bidRepository.save(bid);
+        bidProducerService.sendPostEvent(bidToPrice(bid));
     }
 
     // delivery 로 event 넘겨주기
     // post 로 event 넘겨주기
     //TODO : error exception
-    private Bid eventToBid(PostEvent.Close event){
+    private Bid eventToBid(PostEvent.Close event) {
         Integer amount = bidChatRepository.getHighestPrice(bidChatKey + event.getPostId())
                 .orElseThrow(RuntimeException::new)
                 .getAmount();
-        return Bid.create(event.getPostId(),event.getWriterId(),amount);
+        return Bid.create(event.getPostId(), event.getWriterId(), amount);
+    }
+
+    private PostEvent.Price bidToPrice(Bid bid) {
+        return PostEvent.Price.of(bid.getId(), bid.getSuccessfulBidPrice());
     }
 
 }
---
+++ bid/src/main/java/com/berry/bid/infrastructure/service/message/BidProducerServiceImpl.java
@@ -0,0 +1,20 @@
+package com.berry.bid.infrastructure.service.message;
+
+import com.berry.bid.application.model.event.PostEvent;
+import com.berry.bid.application.service.message.BidProducerService;
+import com.berry.bid.infrastructure.model.enums.KafkaTopic;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BidProducerServiceImpl implements BidProducerService {
+
+    private final KafkaTemplate<String,Object> kafkaTemplate;
+
+    @Override
+    public void sendPostEvent(PostEvent.Price event) {
+        kafkaTemplate.send(KafkaTopic.POST_EVENTS.getTopicName(), event);
+    }
+}
