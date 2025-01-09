75ef955188ac020de16fe68a079664ceb0522a8a
drinkgalaxy <drinkgalaxy1226@gmail.com>
2025-01-09 21:45:50 +0900

    fix:  post kafka 수신 파일 수정 (#85)

--- post/src/main/java/com/berry/post/infrastructure/config/KafkaConsumerConfig.java
+++ post/src/main/java/com/berry/post/infrastructure/config/KafkaConsumerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @EnableKafka
 @Configuration
@@ -26,7 +27,7 @@ public class KafkaConsumerConfig {
     configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaPort);
     configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "post-group");
     configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+    configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
     configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
     return new DefaultKafkaConsumerFactory<>(configProps);
   }
