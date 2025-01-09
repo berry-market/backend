af7707b1c7d2bd0c92152c57ebec7c52b1d74152
sojinchoi <dvega.lyla@gmail.com>
2025-01-09 10:04:51 +0900

    feat: 로그인 UserClient로 user 정보 받기  #64

--- auth/src/main/java/com/berry/auth/infrastructure/security/service/CustomUserDetailsService.java
+++ auth/src/main/java/com/berry/auth/infrastructure/security/service/CustomUserDetailsService.java
@@ -2,7 +2,7 @@ package com.berry.auth.infrastructure.security.service;
 
 import com.berry.auth.domain.model.Role;
 import com.berry.auth.infrastructure.security.dto.CustomUserDetails;
-import com.berry.auth.infrastructure.security.dto.UserResDto;
+import com.berry.auth.infrastructure.security.dto.UserLoginResDto;
 import com.berry.auth.infrastructure.client.UserClient;
 import com.berry.common.exceptionhandler.CustomApiException;
 import com.berry.common.response.ResErrorCode;
@@ -25,29 +25,25 @@ public class CustomUserDetailsService implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
 
-    UserResDto userResDto;
+    UserLoginResDto user;
 
-     // TODO: User Service 연동
-//    try {
-//      // 유저서비스에서 유저 정보 가져오기
-//      userResDto = userClient.getUserByNickname(nickname).getData();
-//    } catch (FeignException e) {
-//      throw new CustomApiException(ResErrorCode.API_CALL_FAILED,
-//          "User Service: " + e.getMessage());
-//    }
-//
-//    if (userResDto == null) {
-//      throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
-//    }
+    try {
+      // 유저서비스에서 유저 정보 가져오기
+      user = userClient.getUserByNickname(nickname).getData();
+    } catch (FeignException e) {
+      throw new CustomApiException(ResErrorCode.API_CALL_FAILED,
+          "User Service: " + e.getMessage());
+    }
 
-     userResDto = new UserResDto(1L, "user1", Role.MEMBER, "$2a$10$wjcbslxcYk5s4hkHLIdtZOnwfmxDCnH4Y82hRlcLLG9Qq0uCw02aW");
-
+    if (user == null) {
+      throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+    }
       // UserDetails 객체 반환
       return new CustomUserDetails(
-          userResDto.getId(),
-          userResDto.getNickname(),
-          userResDto.getPassword(),
-          new SimpleGrantedAuthority(userResDto.getRole().name())
+          user.getUserId(),
+          user.getNickname(),
+          user.getPassword(),
+          new SimpleGrantedAuthority(user.getRole().name())
       );
   }
 }
