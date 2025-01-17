a03b4a2712915d10cc4f140d6ecf08bdf9ecfba1
유현 <yh7119@gmail.com>
2025-01-17 12:36:46 +0900

    feat : 결제 완료 이벤트 기능 구현

--- user/src/main/java/com/berry/user/application/service/UserConsumerService.java
+++ user/src/main/java/com/berry/user/application/service/UserConsumerService.java
@@ -1,6 +1,7 @@
 package com.berry.user.application.service;
 
 import com.berry.common.response.ResErrorCode;
+import com.berry.user.application.model.event.UserEvent;
 import com.berry.user.domain.model.User;
 import com.berry.user.domain.repository.UserJpaRepository;
 import com.berry.user.infrastructure.model.PaymentCompletedEvent;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -17,8 +19,19 @@ public class UserConsumerService {
     private final UserJpaRepository userJpaRepository;
 
     @KafkaListener(topics = "payment-completed", groupId = "user-service-group")
+    @Transactional
     public void completePayment(PaymentCompletedEvent event) {
         log.info("결제 완료 이벤트 수신. userId={}, amount={}", event.getUserId(), event.getAmount());
+
+        User user = userJpaRepository.findById(event.getUserId())
+            .orElseThrow(() -> new IllegalArgumentException(ResErrorCode.NOT_FOUND.getMessage()));
+
+        user.chargePoint(event.getUserId(), event.getAmount());
+    }
+
+    @KafkaListener(topics = "user-events", groupId = "user-service-group")
+    public void updatePoints(UserEvent.Bidding event) {
+        log.info("입찰 완료 이벤트 수신. userId={}, amount={}", event.getUserId(), event.getAmount());
 
         User user = userJpaRepository.findById(event.getUserId())
             .orElseThrow(() -> new IllegalArgumentException(ResErrorCode.NOT_FOUND.getMessage()));
