5d5f1fe939830fa2f532d870b13ed502a5fb684c
sojinchoi <dvega.lyla@gmail.com>
2025-01-19 17:05:11 +0900

    fix: CORS_ALLOWED_ORIGINS 환경변수 처리 #128

--- gateway/src/main/java/com/berry/gateway/config/GatewayCorsConfig.java
+++ gateway/src/main/java/com/berry/gateway/config/GatewayCorsConfig.java
@@ -1,5 +1,6 @@
 package com.berry.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,18 +10,21 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class GatewayCorsConfig {
 
+  @Value("${cors.allowed-origin}")
+  private String corsAllowedOrigin;
+
   @Bean
   public CorsWebFilter corsWebFilter() {
     CorsConfiguration config = new CorsConfiguration();
-    config.addAllowedOrigin("http://localhost:3000"); // 요청을 허용할 프론트엔드 도메인
-    config.addAllowedHeader("*"); // 모든 헤더 허용
-    config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
-    config.setAllowCredentials(true); // 인증 정보 포함 허용
-    config.addExposedHeader("Authorization"); // 클라이언트에서 접근 가능한 헤더
+    config.addAllowedOrigin(corsAllowedOrigin);
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    config.setAllowCredentials(true);
+    config.addExposedHeader("Authorization");
     config.addExposedHeader("Set-Cookie");
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config); // 모든 경로에 대해 CORS 설정 적용
+    source.registerCorsConfiguration("/**", config);
 
     return new CorsWebFilter(source);
   }
